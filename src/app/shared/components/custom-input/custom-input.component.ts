5d2405eea3a814090b721b8c0272d00a3b6406c0
Leoshon <131702139+Leoshon@users.noreply.github.com>
2024-01-03 13:10:52 +0100

    validators added

--- src/app/shared/components/custom-input/custom-input.component.ts
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-input',
   templateUrl: './custom-input.component.html',
   styleUrls: ['./custom-input.component.scss'],
 })
-export class CustomInputComponent  implements OnInit {
+export class CustomInputComponent implements OnInit {
+  @Input() control!: FormControl;
+  @Input() label!: string;
+  @Input() type!: string;
+  @Input() placeholder!: string;
+  @Input() icon!: string;
+  @Input() autocomplete!: string;
+  isPassword!: boolean;
+  hidePassword: boolean = true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    if (this.type === 'password') {
+      this.isPassword = true;
+    }
+  }
+  showHidePassword() {
+    this.hidePassword = !this.hidePassword;
+    if (this.hidePassword) {
+      this.type = 'password';
+    }else{
+      this.type = 'text';
+    }
+  }
 }
